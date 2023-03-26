0b1ebdf35e31e71b68e7a328d7e650c0e6eb9d72
yingmu <wawamu2201@gmail.com>
2023-03-26 23:53:05 +0200

    functions around questions and commenting

--- config/route.js
+++ config/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const apps = require('../controller/userControl');
-const control = require('../controller/questionControl')
+const control = require('../controller/questionControl');
 const auth = require('../middleware/authentication');
 
 
@@ -12,6 +12,15 @@ router.get('/auth', apps.showAuthPage);
 router.get('/logOut', apps.loggingOut);
 router.post('/signUp', apps.signUp);
 router.post('/logIn', apps.logIn);
+
+router.get('/addQuestion', auth.checkToken, control.addQuestion)
+router.post('/postQuestion', control.postQuestion)
+
+router.all('/question/:id', control.viewQuestion)
+
+router.all('/question/edit/:id', control.editQuestion)
+router.get('/question/delete/:id', control.deleteQuestion)
+router.get('/comment/delete/:id', control.deleteComment)
 
 
 module.exports = router;
